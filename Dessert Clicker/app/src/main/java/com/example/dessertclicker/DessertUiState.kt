b85eebd58a02c631bf768c7614acb1a3d4e40bd9
kipkoechke <chepkwonyke1@gmail.com>
2024-03-20 16:38:35 +0300

    Implemented the dessert ui state

--- Dessert Clicker/app/src/main/java/com/example/dessertclicker/DessertUiState.kt
+++ Dessert Clicker/app/src/main/java/com/example/dessertclicker/DessertUiState.kt
@@ -1,3 +1,12 @@
 package com.example.dessertclicker
 
-data class DessertUiState()
+import androidx.annotation.DrawableRes
+import com.example.dessertclicker.data.Datasource.dessertList
+
+data class DessertUiState(
+    var revenue: Int = 0,
+    val dessertsSold: Int = 0,
+    val currentDessertIndex: Int = 0,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
+    @DrawableRes val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId,
+)
