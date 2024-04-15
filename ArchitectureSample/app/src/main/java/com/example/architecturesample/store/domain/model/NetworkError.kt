f59772ad09ac6f5bae230fcb2468c23ffed59c62
kipkoechke <chepkwonyke1@gmail.com>
2024-04-15 20:00:47 +0300

    Chore: Added repository for abstraction of product

---
+++ ArchitectureSample/app/src/main/java/com/example/architecturesample/store/domain/model/NetworkError.kt
@@ -0,0 +1,13 @@
+package com.example.architecturesample.store.domain.model
+
+data class NetworkError(
+    val error: ApiError,
+    val t: Throwable? = null
+
+)
+
+enum class ApiError(val message: String) {
+    NetworkError("Network Error"),
+    UnknownResponse("Unknown Response"),
+    UnknownError("Unknown Error")
+}
