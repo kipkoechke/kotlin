d7b81d9d0546717334a225d77e00ea882b817585
kipkoechke <chepkwonyke1@gmail.com>
2024-04-16 16:53:58 +0300

    Chore: Implemented products repository

--- ArchitectureSample/app/src/main/java/com/example/architecturesample/store/domain/repository/ProductsRepository.kt
+++ ArchitectureSample/app/src/main/java/com/example/architecturesample/store/domain/repository/ProductsRepository.kt
@@ -5,7 +5,5 @@ import com.example.architecturesample.store.domain.model.NetworkError
 import com.example.architecturesample.store.domain.model.Product
 
 interface ProductsRepository {
-
     suspend fun getProducts(): Either<NetworkError, List<Product>>
-
 }
