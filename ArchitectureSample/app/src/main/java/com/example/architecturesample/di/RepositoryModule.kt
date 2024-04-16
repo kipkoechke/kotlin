129a009386acfb10102235910f54465009c732ce
kipkoechke <chepkwonyke1@gmail.com>
2024-04-16 16:50:58 +0300

    Chore: Implemented repository dependency injection

---
+++ ArchitectureSample/app/src/main/java/com/example/architecturesample/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.example.architecturesample.di
+
+import com.example.architecturesample.store.data.repository.ProductsRepositoryImpl
+import com.example.architecturesample.store.domain.repository.ProductsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindProductsRepository(impl: ProductsRepositoryImpl): ProductsRepository
+}
