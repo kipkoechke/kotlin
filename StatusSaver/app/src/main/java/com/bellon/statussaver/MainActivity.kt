7fb2f3fcd1223c15c6120121519d58c2fce62dda
kipkoechke <chepkwonyke1@gmail.com>
2024-05-23 16:26:09 +0300

    Add main activity

--- StatusSaver/app/src/main/java/com/bellon/statussaver/MainActivity.kt
+++ StatusSaver/app/src/main/java/com/bellon/statussaver/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bellon.statussaver.presentations.TabScreen
 import com.bellon.statussaver.ui.theme.StatusSaverTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StatusSaverTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 8.dp),
+                ) {
+                    TabScreen(modifier = Modifier.padding(top = it.calculateTopPadding()))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StatusSaverTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
