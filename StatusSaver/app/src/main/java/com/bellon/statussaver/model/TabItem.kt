c718f4f95c4d0dd8579e008425876570dfc75893
kipkoechke <chepkwonyke1@gmail.com>
2024-05-23 16:18:21 +0300

    Add tab item

---
+++ StatusSaver/app/src/main/java/com/bellon/statussaver/model/TabItem.kt
@@ -0,0 +1,9 @@
+package com.bellon.statussaver.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class TabItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
