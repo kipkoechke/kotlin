a59c3f50c5ac03b03f3baea099e828de4ad0d06b
kipkoechke <chepkwonyke1@gmail.com>
2024-05-23 16:15:55 +0300

    Add coil library

--- StatusSaver/app/build.gradle.kts
+++ StatusSaver/app/build.gradle.kts
@@ -75,4 +75,7 @@ dependencies {
     implementation ("androidx.documentfile:documentfile:1.0.1")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+
+    // Coil
+    implementation(libs.io.coil.compose)
 }
