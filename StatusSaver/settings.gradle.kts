25337bb540941d6f3601d18a679384a0fca1b63d
kipkoechke <chepkwonyke1@gmail.com>
2024-05-30 21:49:49 +0300

    Add dependencies and request permissions

--- StatusSaver/settings.gradle.kts
+++ StatusSaver/settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode.*
+
 pluginManagement {
     repositories {
         google {
@@ -11,11 +13,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
     }
 }
 
